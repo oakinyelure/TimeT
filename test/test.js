d68139caccd3db8ee995eb871cea3111e430addf
Olusegun Akinyelure <oakinyelure@hotmail.com>
2019-06-21 16:45:21 -0400

    updated test

--- test/test.js
+++ test/test.js
@@ -3,21 +3,21 @@ const expect = require("chai").expect;
 const TimeT = require("../TimeT.js");
 
 describe("#Class Instantiation", () => {
-    it('empty instantiation should create instance of TimeT', () => {
+    it('empty instantiation should create instance of TimeT', function() {
         expect(new TimeT()).to.be.an.instanceof(TimeT);
     }),
 
-    it('string instantiation should create instance of TimeT', () => {
+    it('string instantiation should create instance of TimeT', function()  {
         expect(new TimeT("2017-05-01")).to.be.an.instanceof(TimeT);
     }),
     
-    it("date instantiation should create instance of TimeT", () => {
+    it("date instantiation should create instance of TimeT", function() {
         expect(new TimeT(new Date)).to.be.an.instanceOf(TimeT);
     })
 });
 
 
-describe("#Instantiating with unsupported date argument", () => {
+describe("#Instantiating with unsupported date argument", function() {
     it("unsupported string should throw an exception", ()=> {
         let wrongStringInstantiation = () => { new TimeT("hgdfhdgjff") };
         expect(wrongStringInstantiation).to.throw();
@@ -28,6 +28,24 @@ describe("#Instantiating with unsupported date argument", () => {
         expect(wrongStringInstantiation).to.not.throw();
     })
 });
+
+describe("#Date property", function() {
+    let instanceDate = new Date("2014-05-04");
+    let newTime = new TimeT(instanceDate);
+    it("should not have an accessible date property", function() {
+        expect((newTime.date == null)).to.be.true;
+    }),
+
+    it("private date property should equal date set from instance", function() {
+        expect(newTime.getTimeInstance()).to.equal(instanceDate);
+    }),
+
+    it("date instance should equal instance set by setDate property", function() {
+        let newDate = new Date("2018-01-05");
+        newTime.setDate(newDate);
+        expect(newTime.getTimeInstance()).to.equal(newDate);
+    })
+})
 
 const Validators = require("./validator-test");
 const Helpers = require("./helper-test");
